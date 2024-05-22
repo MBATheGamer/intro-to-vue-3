14124d579a52ba674b528faaf64e819470ac4fda
MBATheGamer <mbathegamer@gmail.com>
2024-05-23 00:11:24 +0100

    Attribute binding (start)

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description:
-        "A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.",
     };
   },
 });
