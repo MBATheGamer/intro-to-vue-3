9ba5b55aa9a393910fdea1b235e3033693ed552e
MBATheGamer <mbathegamer@gmail.com>
2024-05-23 00:01:16 +0100

    Create the Vue app (Code Challenge)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
+      description:
+        "A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.",
     };
   },
 });
